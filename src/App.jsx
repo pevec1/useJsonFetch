cfd6a18591d111a3248bd97ada124e9a156cd422
Andrei Harenkov <pevec1@yandex.ru>
2024-06-09 12:57:49 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+//import { useState } from 'react'
+import './App.css'
+import { useJsonFetch } from './hooks/useJsonFetch'
+
+function App() {
+
+  const [data, error, isLoading] = useJsonFetch("http://localhost:7071/data", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const [data2, error2, isLoading2] = useJsonFetch(
+    "http://localhost:7071/error",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const [data3, error3, isLoading3] = useJsonFetch(
+    "http://localhost:7071/loading",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  console.log(data, error, isLoading);
+  console.log(data2, error2, isLoading2);
+  console.log(data3, error3, isLoading3);
+
+  return (
+    <>
+      {data !== null && JSON.stringify(data) ? JSON.stringify(data) : "loading"}
+      <br />
+      {data2 !== null && JSON.stringify(data2) ? JSON.stringify(data2) : "loading"}
+      <br />
+      {data3 !== null && JSON.stringify(data3) ? JSON.stringify(data3) : "loading"}
+    </>
+  );
+}
+
+export default App
---
+++ src/hooks/useJsonFetch.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect, useRef } from 'react'
+
+export const useJsonFetch = (url, opts) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const timestampRef = useRef(0);
+
+  function loadData(){
+    setIsLoading(true);
+    setTimeout(() => {
+      timestampRef.current = Date.now();
+    fetch(url, opts)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setError(null);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+    }, 2000);
+  }
+  useEffect(loadData); // componentDidMount
+
+  return [data, error, isLoading];
+}
