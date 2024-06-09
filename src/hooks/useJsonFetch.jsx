c28d1f24fb9ccc34ff88e99d0975e2a0e7229d2c
Andrei Harenkov <pevec1@yandex.ru>
2024-06-09 19:20:04 +0300

    update commit 09-06-2024

--- src/hooks/useJsonFetch.jsx
+++ src/hooks/useJsonFetch.jsx
@@ -23,7 +23,7 @@ export const useJsonFetch = (url, opts) => {
         setError(err);
         setIsLoading(false);
       });
-    }, 100);
+    }, 2000);
   }
   useEffect(loadData, [url, opts]); // componentDidMount
 
